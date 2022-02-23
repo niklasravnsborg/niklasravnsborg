599c3814e4648fe84ace1ed2a78e1a520a06beea
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2022-02-23 19:53:44 +0100

    adjust Link component

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,3 +1,15 @@
-import { Link } from 'theme-ui'
+import React from 'react'
+import { Link as ThemeUILink } from 'theme-ui'
+import { Link as GatsbyLink } from 'gatsby'
+
+const Link = ({ to, ...props }) => {
+  const isInternal = /^\/(?!\/)/.test(to)
+
+  return isInternal ? (
+    <GatsbyLink to={to} {...props} />
+  ) : (
+    <ThemeUILink href={to} {...props} />
+  )
+}
 
 export default Link
