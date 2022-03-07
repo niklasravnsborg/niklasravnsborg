135fceb2dd603a0697cc7ecec3714ceec0d7ffa1
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2022-03-07 11:52:52 +0100

    improve loading state for VideoCard

--- src/components/VideoCard/index.js
+++ src/components/VideoCard/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Box, Card, Text, AspectImage } from 'theme-ui'
+import { shade } from 'polished'
+import { Box, Card, Text, AspectRatio, Image } from 'theme-ui'
 
 import Ellipsis from '../Ellipsis'
 
 const VideoCard = ({ youtubeId, color, name, date, bright }) => (
   <a href={`https://youtu.be/${youtubeId}`} target="_blank" rel="noreferrer">
     <Card p={3} bg={color}>
-      <AspectImage
-        ratio={16 / 9}
-        src={`https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg?fo2`}
-        alt={name}
-      />
+      <AspectRatio ratio={16 / 9} bg={shade(0.2, color)}>
+        <Image
+          alt={name}
+          src={`https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`}
+        />
+      </AspectRatio>
       <Box mt="3" color={bright && 'dark'}>
         <Ellipsis sx={{ fontWeight: '500' }}>{name}</Ellipsis>
         <Text sx={{ fontSize: '0.9em', fontStyle: 'italic', opacity: '.6' }}>
